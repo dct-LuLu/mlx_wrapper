24d8ed62813a664f166a2a157a0037d1f7c641ba
ketodin-cachyos <jaubry--@student.42lyon.fr>
2025-09-09 10:02:20 +0200

    EDIT: updated mouse to be aligned with library-stripped

--- src/mouse/mouse.c
+++ src/mouse/mouse.c
@@ -6,7 +6,7 @@
 /*   By: jaubry-- <jaubry--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 02:52:27 by jaubry--          #+#    #+#             */
-/*   Updated: 2025/09/09 02:10:26 by jaubry--         ###   ########.fr       */
+/*   Updated: 2025/09/09 09:58:13 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,42 @@ static void	mouse_move_focus(t_mlx *mlx_data)
 	mlx_mouse_move(mlx_data->mlx, mlx_data->win,
 			mlx_data->half_size.x, mlx_data->half_size.y);
 	XFlush(mlx_data->mlx->display);
+	mlx_data->mouse_input.last_pos = mlx_data->half_size;
 }
 
-static void	check_mouse_focus_state(t_mlx *mlx_data)
+static void	update_mouse_focus_state(void *v, t_mlx *mlx_data)//a ameliorer
 {
 	bool	focus_mode = mlx_data->mouse_input.focus;
 
+	(void)v;
 	if (focus_mode)
 	{
 		mlx_mouse_hide(mlx_data->mlx, mlx_data->win);
 		XGrabPointer(mlx_data->mlx->display, mlx_data->win->window,
 				True, PointerMotionMask, GrabModeAsync, GrabModeAsync,
-				mlx_data->win->window, None, CurrentTime);
+				None, None, CurrentTime);
+		mlx_data->mouse_input.pos = mlx_data->half_size;
+		mouse_move_focus(mlx_data);
 	}
 	else
 	{
 		mlx_mouse_show(mlx_data->mlx, mlx_data->win);
 		XUngrabPointer(mlx_data->mlx->display, CurrentTime);
+	}
+}
+
+static void	mouse_move_action(t_mlx *mlx_data)
+{
+	t_mouse_event	*mouse_event;
+	size_t			i;
+
+	i = 0;
+	while (i < mlx_data->mouse_input.move_events->num_elements)
+	{
+		mouse_event = get_vector_value(mlx_data->mouse_input.move_events, i);
+		if (mouse_event->action)
+			mouse_event->action(mouse_event->arg, mlx_data);
+		i++;
 	}
 }
 
@@ -45,20 +64,22 @@ static int	mouse_move(int x, int y, t_mlx *mlx_data)
 		mlx_data->mouse_input.warped = false;
 		return (0);
 	}
-	check_mouse_focus_state(mlx_data);
 	if (mlx_data->mouse_input.focus)
 		mouse_move_focus(mlx_data);
-	printf("last x:%d y:%d\t\tcur x:%d y:%d\n", mlx_data->mouse_input.last_pos.x,
-			mlx_data->mouse_input.last_pos.y, mlx_data->mouse_input.pos.x, mlx_data->mouse_input.pos.y);
-	mlx_data->mouse_input.last_pos.x = mlx_data->mouse_input.pos.x;
-	mlx_data->mouse_input.last_pos.y = mlx_data->mouse_input.pos.y;
+	else
+		mlx_data->mouse_input.last_pos = mlx_data->mouse_input.pos;
 	mlx_data->mouse_input.pos.x = x;
 	mlx_data->mouse_input.pos.y = y;
+	mouse_move_action(mlx_data);
 	return (0);
 }
 
 static int	setup_mouse_movement(t_mlx *mlx_data)
 {
+	mlx_data->mouse_input.move_events = ft_calloc(1, sizeof(t_vector));
+	if (!mlx_data->mouse_input.move_events)
+		return (-1);
+	vector_init(mlx_data->mouse_input.move_events, sizeof(t_mouse_event));
 	mlx_hook(mlx_data->win, MotionNotify,
 			PointerMotionMask, mouse_move, mlx_data);
 	return (0);
