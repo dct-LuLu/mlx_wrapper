6a9cbc9dc9f7ed9f25c4b3cdb1c78dcd536e4dfc
ketodin-cachyos <jaubry--@student.42lyon.fr>
2025-09-09 09:40:10 +0200

    UPDATE: from main branch the key_input system changes

--- src/keys/keys.c
+++ src/keys/keys.c
@@ -6,54 +6,36 @@
 /*   By: jaubry-- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 23:08:29 by jaubry--          #+#    #+#             */
-/*   Updated: 2025/09/08 23:16:01 by jaubry--         ###   ########.fr       */
+/*   Updated: 2025/09/09 09:37:17 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx_wrapper.h"
-
-static void	key_action(t_mlx *mlx_data)
-{
-	t_key_event	*key_event;
-	size_t		i;
-
-	i = 0;
-	while (i < mlx_data->key_input.key_events->num_elements)
-	{
-		key_event = get_vector_value(mlx_data->key_input.key_events, i);
-		if (key_event->action && key_event->is_key(mlx_data->key_input.keycode))
-			key_event->action(key_event->arg, mlx_data);
-		i++;
-	}
-}
 
 static int	key_press(int keycode, t_mlx *mlx_data)
 {
 	t_key_event	*key_event;
 	size_t		i;
 
-	if (is_escape_key(keycode))
-		mlx_loop_end(mlx_data->mlx);
-	if (RESIZEABLE && (keycode == XK_F11))
-	{
-		mlx_data->fullscreen = !mlx_data->fullscreen;
-		mlx_ext_fullscreen(mlx_data->mlx, mlx_data->win, mlx_data->fullscreen);
-	}
 	mlx_data->key_input.keycode = keycode;
 	i = 0;
 	while (i < mlx_data->key_input.key_events->num_elements)
 	{
 		key_event = get_vector_value(mlx_data->key_input.key_events, i);
-		if (key_event->is_key(keycode) && key_event->status)
+		if (key_event->is_key(keycode))
 		{
-			if (key_event->toggle)
-				*(key_event->status) = !*(key_event->status);
-			else
-				*(key_event->status) = true;
+			if (key_event->action)
+				key_event->action(key_event->arg, mlx_data);
+			else if (key_event->status)
+			{
+				if (key_event->toggle)
+					*(key_event->status) = !*(key_event->status);
+				else
+					*(key_event->status) = true;
+			}
 		}
 		i++;
 	}
-	key_action(mlx_data);
 	return (0);
 }
 
@@ -67,44 +49,67 @@ static int	key_release(int keycode, t_mlx *mlx_data)
 	while (i < mlx_data->key_input.key_events->num_elements)
 	{
 		key_event = get_vector_value(mlx_data->key_input.key_events, i);
-		if (!(key_event->toggle) && key_event->is_key(keycode) && key_event->status)
+		if (!(key_event->toggle) && key_event->is_key(keycode)
+				&& key_event->status)
 			*(key_event->status) = false;
 		i++;
 	}
-	key_action(mlx_data);
 	return (0);
+}
+
+void	add_status_key_hook(t_mlx *mlx_data, bool (*is_key)(int),
+		bool toggle, bool *status)
+{
+	const t_key_event	key_event = (t_key_event)
+	{
+		.is_key = is_key,
+		.toggle = toggle,
+		.status = status
+	};
+	vector_add(mlx_data->key_input.key_events, (void *)&key_event, 1);
+}
+
+void	add_func_key_hook(t_mlx *mlx_data, bool (*is_key)(int),
+		void (*action)(void *, t_mlx *), void *arg)
+{
+	const t_key_event	key_event = (t_key_event)
+	{
+		.is_key = is_key,
+		.action = action,
+		.arg = arg
+	};
+	vector_add(mlx_data->key_input.key_events, (void *)&key_event, 1);
+}
+
+static void	ft_mlx_fullscreen_toggle(void *v, t_mlx *mlx_data)
+{
+	(void)v;
+	if (RESIZEABLE)
+	{
+		mlx_data->fullscreen = !mlx_data->fullscreen;
+		mlx_ext_fullscreen(mlx_data->mlx, mlx_data->win, mlx_data->fullscreen);
+	}
+}
+
+static void	mlx_exit(void *v, t_mlx *mlx_data)
+{
+	(void)v;
+	mlx_loop_end(mlx_data->mlx);
+}
+
+static bool	is_f11_key(int keycode)
+{
+	return (keycode == XK_F11);
 }
 
 //check return err;
 static void	setup_special_key_events(t_mlx *mlx_data)
 {
-	t_key_event	special_key_events[3];
-
-	special_key_events[0] = (t_key_event)
-	{
-		.is_key = is_ctrl_key,
-		.action = NULL,
-		.arg = NULL,
-		.toggle = false,
-		.status = &(mlx_data->key_input.ctrl)
-	};
-	special_key_events[1] = (t_key_event)
-	{
-		.is_key = is_shift_key,
-		.action = NULL,
-		.arg = NULL,
-		.toggle = false,
-		.status = &(mlx_data->key_input.shift)
-	};
-	special_key_events[2] = (t_key_event)
-	{
-		.is_key = is_caps_key,
-		.action = NULL,
-		.arg = NULL,
-		.toggle = true,
-		.status = &(mlx_data->key_input.caps)
-	};
-	vector_add(mlx_data->key_input.key_events, special_key_events, 3);
+	add_status_key_hook(mlx_data, is_ctrl_key, false, &(mlx_data->key_input.ctrl));
+	add_status_key_hook(mlx_data, is_shift_key, false, &(mlx_data->key_input.shift));
+	add_status_key_hook(mlx_data, is_caps_key, true, &(mlx_data->key_input.caps));
+	add_func_key_hook(mlx_data, is_escape_key, mlx_exit, NULL);
+	add_func_key_hook(mlx_data, is_f11_key, ft_mlx_fullscreen_toggle, NULL);
 }
 
 int	setup_key_input(t_mlx *mlx_data)
@@ -115,7 +120,8 @@ int	setup_key_input(t_mlx *mlx_data)
 	vector_init(mlx_data->key_input.key_events, sizeof(t_key_event));
 	mlx_hook(mlx_data->win, KeyPress, KeyPressMask, key_press, mlx_data);
 	mlx_hook(mlx_data->win, KeyRelease, KeyReleaseMask, key_release, mlx_data);
-	mlx_hook(mlx_data->win, DestroyNotify, StructureNotifyMask, mlx_loop_end, mlx_data->mlx);
+	mlx_hook(mlx_data->win, DestroyNotify, StructureNotifyMask,
+			mlx_loop_end, mlx_data->mlx);
 	setup_special_key_events(mlx_data);
 	return (0);
 }
