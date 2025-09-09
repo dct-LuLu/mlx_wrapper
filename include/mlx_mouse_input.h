3fc9020e911d4edc59d385d9f57d3a90be68bad5
ketodin-cachyos <jaubry--@student.42lyon.fr>
2025-09-09 04:16:34 +0200

    god why does this always happen with git submodules huhhhh

--- include/mlx_mouse_input.h
+++ include/mlx_mouse_input.h
@@ -6,19 +6,28 @@
 /*   By: jaubry-- <jaubry--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 02:52:34 by jaubry--          #+#    #+#             */
-/*   Updated: 2025/09/08 23:14:34 by jaubry--         ###   ########.fr       */
+/*   Updated: 2025/09/09 04:14:54 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MLX_MOUSE_INPUT_H
 # define MLX_MOUSE_INPUT_H
 
+void	update_mouse_focus_state(void *v, t_mlx *mlx_data);
+
+typedef struct s_mouse_event
+{
+	void	(*action)(void *, t_mlx *);
+	void	*arg;
+}			t_mouse_event;
+
 typedef struct s_mouse_input
 {
-	t_vec2i	pos;
-	t_vec2i	last_pos;
-	bool	warped;
-	bool	focus;
+	t_vector	*move_events;
+	t_vec2i		pos;
+	t_vec2i		last_pos;
+	bool		warped;
+	bool		focus;
 }			t_mouse_input;
 
 #endif//MLX_MOUSE_INPUT_H
