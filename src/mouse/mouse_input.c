0194f6c77c406a54a8ae67a191e382d6293a4804
jaubry-- <jaubry--@student.42lyon.fr>
2025-10-14 21:34:01 +0200

    ADD: adding mouse input handling

--- src/mouse/mouse_input.c
+++ src/mouse/mouse_input.c
@@ -1,20 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   mouse.c                                            :+:      :+:    :+:   */
+/*   mouse_input.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaubry-- <jaubry--@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/08 02:52:27 by jaubry--          #+#    #+#             */
-/*   Updated: 2025/10/14 21:25:57 by jaubry--         ###   ########.fr       */
+/*   Created: 2025/10/14 21:26:12 by jaubry--          #+#    #+#             */
+/*   Updated: 2025/10/14 21:28:41 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx_wrapper.h"
 
-int	setup_mouse_movement(t_mlx *mlx_data);
+static int mouse_event(int x, int y, int click, t_mlx *mlx_data)
+{
+    
+}
 
 int	setup_mouse_input(t_mlx *mlx_data)
 {
-	return (setup_mouse_movement(mlx_data));
+    mlx_hook(mlx_data->win, MotionNotify,
+                PointerMotionMask, mouse_move, mlx_data);
 }
