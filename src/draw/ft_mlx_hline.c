7e5fb9f39ea53859b3bf267c7f1ff338be17891b
jaubry-- <jaubry--@student.42lyon.fr>
2025-10-15 20:27:25 +0200

    NORM: normed hline and vline

--- src/draw/ft_mlx_hline.c
+++ src/draw/ft_mlx_hline.c
@@ -6,7 +6,7 @@
 /*   By: jaubry-- <jaubry--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 02:24:53 by jaubry--          #+#    #+#             */
-/*   Updated: 2025/10/15 03:02:23 by jaubry--         ###   ########.fr       */
+/*   Updated: 2025/10/15 20:26:10 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,55 @@
 #define START 0
 #define END 1
 
-void ft_mlx_hline(t_img_data *img, const int xpt[2], const int y, const int color)
+static inline void	ft_mlx_span(uint32_t *p,
+		int width, const int color)
 {
-    int start_x, end_x, width;
-    uint8_t *row;
+	int	rem;
+	int	n;
 
-    if (!img || !img->addr || img->line_len <= 0 || img->byte_depth <= 0)
-        return;
-    if (y < 0 || y >= img->height)
-        return;
+	n = width >> 3;
+	while (n--)
+	{
+		p[0] = color;
+		p[1] = color;
+		p[2] = color;
+		p[3] = color;
+		p[4] = color;
+		p[5] = color;
+		p[6] = color;
+		p[7] = color;
+		p += 8;
+	}
+	rem = width & 7;
+	while (rem--)
+	{
+		*p++ = color;
+	}
+}
 
-    start_x = xpt[!(xpt[START] < xpt[END])];
-    end_x   = xpt[(xpt[START] < xpt[END])];
-    if (start_x < 0) start_x = 0;
-    if (end_x >= img->width) end_x = img->width - 1;
+void	ft_mlx_hline(t_img_data *img, const int xpt[2],
+			const int y, const int color)
+{
+	int		start_x;
+	int		end_x;
+	int		width;
+	uint8_t	*row;
 
-    width = end_x - start_x + 1;
-    if (width <= 0)
-        return;
-
-    row = (uint8_t *)img->addr
-        + (size_t)y * (size_t)img->line_len
-        + (size_t)start_x * (size_t)img->byte_depth;
-
-    uint32_t c = (uint32_t)color;
-    uint32_t *p32 = (uint32_t *)row;
-
-    int n = width >> 3;      // blocks of 8 pixels
-    while (n--)
-    {
-        p32[0] = c; p32[1] = c; p32[2] = c; p32[3] = c;
-        p32[4] = c; p32[5] = c; p32[6] = c; p32[7] = c;
-        p32 += 8;
-    }
-    int rem = width & 7;
-    while (rem--) { *p32++ = c; }
+	if (!img || !img->addr || img->line_len <= 0 || img->byte_depth <= 0)
+		return ;
+	if (y < 0 || y >= img->height)
+		return ;
+	start_x = xpt[!(xpt[START] < xpt[END])];
+	end_x = xpt[(xpt[START] < xpt[END])];
+	if (start_x < 0)
+		start_x = 0;
+	if (end_x >= img->width)
+		end_x = img->width - 1;
+	width = end_x - start_x + 1;
+	if (width <= 0)
+		return ;
+	row = (uint8_t *)img->addr
+		+ (size_t)y * (size_t)img->line_len
+		+ (size_t)start_x * (size_t)img->byte_depth;
+	ft_mlx_span((uint32_t *)row, width, color);
 }
--- src/draw/ft_mlx_vline.c
+++ src/draw/ft_mlx_vline.c
@@ -6,7 +6,7 @@
 /*   By: jaubry-- <jaubry--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 02:26:18 by jaubry--          #+#    #+#             */
-/*   Updated: 2025/10/15 03:04:04 by jaubry--         ###   ########.fr       */
+/*   Updated: 2025/10/15 20:24:34 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,44 +15,55 @@
 #define START 0
 #define END 1
 
-void ft_mlx_vline(t_img_data *img, const int ypt[2],
-		const int x, const int color)
+static inline void	ft_mlx_span(t_img_data *img, uint8_t *p,
+		int height, const int color)
 {
-    int start_y, end_y, height;
-    uint8_t *p;
+	int	n;
+	int	rem;
 
-    if (!img || !img->addr || img->line_len <= 0 || img->byte_depth <= 0)
-        return;
-    if (x < 0 || x >= img->width)
-        return;
+	n = height >> 2;
+	while (n--)
+	{
+		*(uint32_t *)(p) = color;
+		p += img->line_len;
+		*(uint32_t *)(p) = color;
+		p += img->line_len;
+		*(uint32_t *)(p) = color;
+		p += img->line_len;
+		*(uint32_t *)(p) = color;
+		p += img->line_len;
+	}
+	rem = height & 3;
+	while (rem--)
+	{
+		*(uint32_t *)(p) = color;
+		p += img->line_len;
+	}
+}
 
-    start_y = ypt[!(ypt[START] < ypt[END])];
-    end_y = ypt[(ypt[START] < ypt[END])];
-    if (start_y < 0)
-    	start_y = 0;
-    if (end_y >= img->height)
-    	end_y = img->height - 1;
+void	ft_mlx_vline(t_img_data *img, const int ypt[2],
+			const int x, const int color)
+{
+	int		start_y;
+	int		end_y;
+	int		height;
+	uint8_t	*p;
 
-    height = end_y - start_y + 1;
-    if (height <= 0)
-        return;
-
-    p = (uint8_t *)img->addr
-      + (size_t)start_y * (size_t)img->line_len
-      + (size_t)x * (size_t)img->byte_depth;
-
-    uint32_t c = (uint32_t)color;
-    int n = height >> 2;     // blocks of 4 pixels
-    while (n--)
-    {
-        *(uint32_t *)(p) = c; p += img->line_len;
-        *(uint32_t *)(p) = c; p += img->line_len;
-        *(uint32_t *)(p) = c; p += img->line_len;
-        *(uint32_t *)(p) = c; p += img->line_len;
-    }
-    int rem = height & 3;
-    while (rem--) 
-    {
-    	*(uint32_t *)(p) = c; p += img->line_len;
-    }
+	if (!img || !img->addr || img->line_len <= 0 || img->byte_depth <= 0)
+		return ;
+	if (x < 0 || x >= img->width)
+		return ;
+	start_y = ypt[!(ypt[START] < ypt[END])];
+	end_y = ypt[(ypt[START] < ypt[END])];
+	if (start_y < 0)
+		start_y = 0;
+	if (end_y >= img->height)
+		end_y = img->height - 1;
+	height = end_y - start_y + 1;
+	if (height <= 0)
+		return ;
+	p = (uint8_t *)img->addr
+		+ (size_t)start_y * (size_t)img->line_len
+		+ (size_t)x * (size_t)img->byte_depth;
+	ft_mlx_span(img, p, height, color);
 }
