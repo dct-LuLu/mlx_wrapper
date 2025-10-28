88b4ea9535835374806b4073518df90b25b07de2
jaubry-- <jaubry--@student.42lyon.fr>
2025-10-28 03:07:40 +0100

    UPDATE: includes

--- include/mlx_draw.h
+++ include/mlx_draw.h
@@ -6,47 +6,195 @@
 /*   By: jaubry-- <jaubry--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 23:08:55 by jaubry--          #+#    #+#             */
-/*   Updated: 2025/09/09 00:24:10 by jaubry--         ###   ########.fr       */
+/*   Updated: 2025/10/27 10:54:53 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MLX_DRAW_H
 # define MLX_DRAW_H
 # include "vectors.h"
+# include "colors.h"
 # include "libft.h"
 
 typedef struct s_img_data	t_img_data;
 
-//drawing functions
+// Pixel draw functions
+
 void	ft_mlx_pixel_put(t_img_data *img, const t_vec2i pos,
-			const int color);
+			const t_rgb_int color);
+void	ft_mlx_pixel_aput(t_img_data *img, const t_vec2i pos,
+			const t_rgba_int color);
 
 void	ft_mlx_safe_pixel_put(t_img_data *img, const t_vec2i pos,
-			const int color);
+			const t_rgb_int color);
+void	ft_mlx_safe_pixel_aput(t_img_data *img, const t_vec2i pos,
+			const t_rgba_int color);
+
+// Line draw functions
+
+void	ft_mlx_hline_put(t_img_data *img, const int xpt[2],
+			const int y, const t_rgb_int color);
+void	ft_mlx_hline_aput(t_img_data *img, const int xpt[2],
+			const int y, const t_rgba_int color);
+
+void	ft_mlx_vline_put(t_img_data *img, const int ypt[2],
+			const int x, const t_rgb_int color);
+void	ft_mlx_vline_aput(t_img_data *img, const int ypt[2],
+			const int x, const t_rgba_int color);
 
 typedef struct s_line
 {
-	int				dx;
-	int				dy;
-	int				sx;
-	int				sy;
-	int				err;
-	int				e2;
-}					t_line;
+	int	dx;
+	int	dy;
+	int	sx;
+	int	sy;
+	int	err;
+	int	e2;
+}		t_line;
 
 void	ft_mlx_line_put(t_img_data *img, t_vec2i a, t_vec2i b,
-			const int color);
+			const t_rgb_int color);
+void	ft_mlx_line_aput(t_img_data *img, t_vec2i a, t_vec2i b,
+			const t_rgba_int color);
+void	ft_mlx_line_aputf(t_img_data *img, t_vec2i p[2],
+			int x, t_rgba_int color);
 
-void	ft_mlx_horizontal_line(t_img_data *img, const int xpt[2],
-			const int y, const int color);
+// Rec draw functions
 
+// deprecated ?
 void	ft_mlx_batch_put(t_img_data *img, const t_vec2i pos,
-			const t_vec2i size, const int color);
+			const t_vec2i size, const t_rgb_int color);
+void	ft_mlx_batch_aput(t_img_data *img, const t_vec2i pos,
+			const t_vec2i size, const t_rgba_int color);
+
+void	ft_mlx_out_aarec_put(t_img_data *img, const t_vec2i pos_a,
+			const t_vec2i pos_b, const t_rgb_int color);
+void	ft_mlx_out_aarec_aput(t_img_data *img, const t_vec2i pos_a,
+			const t_vec2i pos_b, const t_rgba_int color);
+
+void	ft_mlx_aarec_put(t_img_data *img, const t_vec2i pos_a,
+			const t_vec2i pos_b, const t_rgb_int color);
+void	ft_mlx_aarec_aput(t_img_data *img, const t_vec2i pos_a,
+			const t_vec2i pos_b, const t_rgba_int color);
+
+// Curves draw functions
+
+void	ft_mlx_out_circle_put(t_img_data *img, const t_vec2i center,
+			const int radius, const t_rgb_int color);
+void	ft_mlx_out_circle_aput(t_img_data *img, const t_vec2i center,
+			const int radius, const t_rgba_int color);
 
 void	ft_mlx_circle_put(t_img_data *img, const t_vec2i center,
-			const int radius, const int color);
+			const int radius, const t_rgb_int color);
+void	ft_mlx_circle_aput(t_img_data *img, const t_vec2i center,
+			const int radius, const t_rgba_int color);
+void	ft_mlx_safe_circle_aput(t_img_data *img, const t_vec2i center,
+			const int radius, const t_rgba_int color);
 
-void	ft_mlx_draw_quadratic_curve(t_img_data *img,
-			const t_vec2i *pts, const int color);
+void	ft_mlx_quad_curve_put(t_img_data *img, const t_vec2i *pts,
+			const t_rgb_int color);
+void	ft_mlx_quad_curve_aput(t_img_data *img, const t_vec2i *pts,
+			const t_rgba_int color);
+void	ft_mlx_quarter_curve_aput(t_img_data *img, const t_vec2i *pts,
+			const t_rgba_int color);
+
+// Special draw functions
+
+void	ft_mlx_select_put(t_img_data *img, const t_vec2i a, const t_vec2i b,
+			const t_rgb_int color);
+
+typedef enum e_border_style
+{
+	NO_BORDER = 0,
+	SOLID,
+	DASHED
+}	t_border_style;
+
+typedef struct s_border
+{
+	uint8_t			size;// px
+	t_rgba_int		color;
+	t_border_style	style;
+}					t_border;
+
+typedef enum e_anchor
+{
+	CENTER,
+	LT,
+	TOP,
+	RT,
+	RIGHT,
+	RB,
+	BOTTOM,
+	LB,
+	LEFT
+}	t_anchor;
+
+typedef enum e_radius_style
+{
+	NO_RADIUS = 0,
+	FULL_PERCENT,
+	FULL_PX,
+	LOCAL_PERCENT,
+	LOCAL_PX
+}	t_radius_style;
+
+typedef struct s_radius
+{
+	t_radius_style	style;
+	union
+	{
+		uint32_t	_mem;
+		struct
+		{
+			uint8_t	lt;
+			uint8_t	rt;
+			uint8_t	lb;
+			uint8_t	rb;
+		};
+		uint8_t	full;
+	};
+}	t_radius;
+
+typedef struct s_corner
+{
+	union
+	{
+		struct
+		{
+			t_vec2i	horz;
+			t_vec2i	corner;
+			t_vec2i	vert;
+			t_vec2i	in;
+		};
+		t_vec2i		_mem[4];
+		t_vec2i		_mem3[3];
+	};
+	t_vec2i	xrec[2];
+	t_vec2i	mrec[2];
+}	t_corner;
+
+typedef struct s_box
+{
+	t_vec2i		pos;
+	t_anchor	anchor;
+	t_vec2i		size;
+	t_rgba_int	color;
+	t_radius	radius;
+	t_border	border;
+
+	// program defined
+	t_vec2		_half_size;
+	t_vec2i		_mid;
+	t_corner	_lt;
+	t_corner	_rt;
+	t_corner	_lb;
+	t_corner	_rb;
+}				t_box;
+
+void	precompute_box_radius(t_box *box);
+int		precompute_box(t_box *box);
+void	draw_border(t_img_data *img, t_box box);
+void	draw_box(t_img_data *img, t_box box);
 
 #endif//MLX_DRAW_H
--- include/mlx_mouse_input.h
+++ include/mlx_mouse_input.h
@@ -6,24 +6,55 @@
 /*   By: jaubry-- <jaubry--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 02:52:34 by jaubry--          #+#    #+#             */
-/*   Updated: 2025/09/09 04:14:54 by jaubry--         ###   ########.fr       */
+/*   Updated: 2025/10/15 02:05:36 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MLX_MOUSE_INPUT_H
 # define MLX_MOUSE_INPUT_H
 
+
+typedef enum e_mbuttons
+{
+	MNULL = 0,
+	MLCLICK,
+	MWHEELCLICK,
+	MRCLICK,
+	MWHEELUP,
+	MWHEELDOWN
+}	t_mbuttons;
+
+typedef enum e_maction
+{
+	MNONE = 0,
+	MPRESS,
+	MRELEASE
+}	t_maction;
+
+typedef struct s_button_event
+{
+	void		(*action)(t_vec2i, t_maction, void *, t_mlx *);
+	void		*arg;
+	bool		*status;
+	t_mbuttons	keycode;
+}				t_button_event;
+
+int	add_func_button_hook(t_mlx *mlx_data, t_mbuttons keycode,
+		void (*action)(t_vec2i, t_maction, void *, t_mlx *), void *arg);
+
 void	update_mouse_focus_state(void *v, t_mlx *mlx_data);
 
-typedef struct s_mouse_event
+typedef struct s_mouse_event//rename move_event
 {
 	void	(*action)(void *, t_mlx *);
 	void	*arg;
 }			t_mouse_event;
 
+
 typedef struct s_mouse_input
 {
 	t_vector	*move_events;
+	t_vector	*button_events;
 	t_vec2i		pos;
 	t_vec2i		last_pos;
 	bool		warped;
